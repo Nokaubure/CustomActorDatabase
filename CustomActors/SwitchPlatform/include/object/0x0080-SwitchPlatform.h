5a81ae799f7b264b43d9e5d147addd8516bb01a5
Nokaubure <ivansoto.80@gmail.com>
2024-09-30 14:59:23 +0200

    Initial commit

---
+++ CustomActors/SwitchPlatform/include/object/0x0080-SwitchPlatform.h
@@ -0,0 +1,13 @@
+#ifndef __MOVINGPLATFORM_H__
+#define __MOVINGPLATFORM_H__
+
+extern u16 gMovingplatform_TexMovingPlatformTop00[];
+extern u16 gMovingplatform_TexMovingPlatformSide00[];
+extern u16 gMovingplatform_TexMovingPlatformBottom00[];
+extern Gfx gMovingplatform_MtlMaterial[];
+extern Gfx gMovingplatform_MtlMaterial001[];
+extern Gfx gMovingplatform_MtlMaterial002[];
+extern CollisionHeader gMovingplatform_CollPlatform[];
+extern Gfx gMovingplatform_DlPlatform[];
+
+#endif /* __MOVINGPLATFORM_H__ */
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Database of custom actors for Ocarina of Time. SharpOcarina 1.53 and above can connect to this database and install actors automatically in z64rom.
+
+Guidelines if you want your custom actor to appear in this database:
+
+TODO
