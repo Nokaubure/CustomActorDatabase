f5e31178a340588f2c0a30327078e9451640b988
Nokaubure <ivansoto.80@gmail.com>
2026-02-13 18:22:18 +0100

    added MM light block

---
+++ CustomActors/LightBlock/src/actor/0x01E4-LightBlock/z_obj_lightblock.c
@@ -0,0 +1,229 @@
+/*
+ * File: z_obj_lightblock.c
+ * Overlay: ovl_Obj_Lightblock
+ * Description: Sun Block
+ */
+
+#include "z_obj_lightblock.h"
+#include "functions.h"
+
+#define ACT_ID 0x0001
+#define OBJ_ID 0x0004
+
+
+#define FLAGS 0x00000000
+
+#define THIS ((ObjLightblock*)thisx)
+
+void ObjLightblock_Init(Actor* thisx, PlayState* play);
+void ObjLightblock_Destroy(Actor* thisx, PlayState* play);
+void ObjLightblock_Update(Actor* thisx, PlayState* play);
+void ObjLightblock_Draw(Actor* thisx, PlayState* play);
+void func_80AF3AC8(ObjLightblock* this);
+void func_80AF3ADC(ObjLightblock* this, PlayState* play);
+void func_80AF3B8C(ObjLightblock* this);
+void func_80AF3BA0(ObjLightblock* this, PlayState* play);
+void func_80AF3C18(ObjLightblock* this);
+void func_80AF3C34(ObjLightblock* this, PlayState* play);
+
+ActorInit Obj_Lightblock_InitVars = {
+    ACT_ID,
+    ACTORCAT_BG,
+    FLAGS,
+    OBJ_ID,
+    sizeof(ObjLightblock),
+    (ActorFunc)ObjLightblock_Init,
+    (ActorFunc)ObjLightblock_Destroy,
+    (ActorFunc)ObjLightblock_Update,
+    (ActorFunc)ObjLightblock_Draw,
+};
+
+static ColliderCylinderInit sCylinderInit = {
+    {
+        COLTYPE_NONE,
+        AT_NONE,
+        AC_ON | AC_TYPE_PLAYER | AC_TYPE_OTHER,
+        OC1_NONE,
+        OC2_NONE,
+        COLSHAPE_CYLINDER,
+    },
+    {
+        ELEMTYPE_UNK0,
+        { 0x00000000, 0x00, 0x00 },
+        { 0x00202000, 0x00, 0x00 },
+        TOUCH_NONE | TOUCH_SFX_NORMAL,
+        BUMP_ON,
+        OCELEM_NONE,
+    },
+    { 84, 120, 0, { 0, 0, 0 } },
+};
+
+typedef struct {
+    /* 0x00 */ f32 scale;
+    /* 0x04 */ s16 radius;
+    /* 0x06 */ s16 height;
+    /* 0x08 */ s16 yShift;
+    /* 0x0C */ s32 params;
+} LightblockTypeVars; // size = 0x10
+
+static LightblockTypeVars sLightblockTypeVars[] = {
+    { 0.1f, 76, 80, 19, 2 },
+    { (1.0f / 6), 126, 144, 19, 3 },
+};
+
+static InitChainEntry sInitChain[] = {
+    ICHAIN_F32(uncullZoneForward, 4000, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneScale, 500, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneDownward, 500, ICHAIN_STOP),
+};
+
+extern Gfx D_801AEF88[];
+extern Gfx D_801AEFA0[];
+
+void func_80AF3910(ObjLightblock* this, PlayState* play) {
+    LightblockTypeVars* typeVars = &sLightblockTypeVars[LIGHTBLOCK_TYPE(&this->dyna.actor)];
+
+    s16 index = Object_GetIndex(&play->objectCtx, OBJECT_EFC_TW);
+    if (index != -1 && Object_IsLoaded(&play->objectCtx, index))
+        Actor_Spawn(&play->actorCtx, play, ACTOR_DEMO_EFFECT, this->dyna.actor.world.pos.x, this->dyna.actor.world.pos.y,this->dyna.actor.world.pos.z, 0, 0, 0, 0x0018);
+}
+
+void ObjLightblock_InitDynaPoly(ObjLightblock* this, PlayState* play, CollisionHeader* collision, s32 flags) {
+    s32 pad;
+    CollisionHeader* colHeader = NULL;
+    s32 pad2;
+
+    DynaPolyActor_Init(&this->dyna, flags);
+    CollisionHeader_GetVirtual(collision, &colHeader);
+    this->dyna.bgId = DynaPoly_SetBgActor(play, &play->colCtx.dyna, &this->dyna.actor, colHeader);
+}
+void ObjLightblock_Init(Actor* thisx, PlayState* play) {
+    s32 pad;
+    ObjLightblock* this = THIS;
+    LightblockTypeVars* typeVars = &sLightblockTypeVars[LIGHTBLOCK_TYPE(&this->dyna.actor)];
+    CollisionHeader* colHeader = NULL;
+
+    ObjLightblock_InitDynaPoly(this, play, gSunBlockCol, DPM_PLAYER);
+
+    Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
+    Actor_SetScale(&this->dyna.actor, typeVars->scale);
+    DynaPolyActor_Init(&this->dyna, 0);
+    Collider_InitCylinder(play, &this->collider);
+    if (Flags_GetSwitch(play, LIGHTBLOCK_DESTROYED(&this->dyna.actor))) {
+        Actor_Kill(&this->dyna.actor);
+        return;
+    }
+
+    //DynaPolyActor_LoadMesh(play, &this->dyna, gSunBlockCol);
+    Collider_SetCylinder(play, &this->collider, &this->dyna.actor, &sCylinderInit);
+    Collider_UpdateCylinder(&this->dyna.actor, &this->collider);
+    this->collider.dim.radius = typeVars->radius;
+    this->collider.dim.height = typeVars->height;
+    this->collider.dim.yShift = typeVars->yShift;
+    this->alpha = 255;
+    func_80AF3AC8(this);
+}
+
+void ObjLightblock_Destroy(Actor* thisx, PlayState* play) {
+    ObjLightblock* this = THIS;
+
+    DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
+    Collider_DestroyCylinder(play, &this->collider);
+}
+
+void func_80AF3AC8(ObjLightblock* this) {
+    this->actionFunc = func_80AF3ADC;
+}
+
+void func_80AF3ADC(ObjLightblock* this, PlayState* play) {
+    if (this->collider.base.acFlags & AC_HIT) {
+        this->collider.base.acFlags &= ~AC_HIT;
+        // light arrows
+        if (this->collider.info.acHitInfo->toucher.dmgFlags & (1 << 13)) {
+            this->collisionCounter = 8;
+        }
+        // light ray
+        else {
+            this->collisionCounter++;
+        }
+    } else {
+        this->collisionCounter = 0;
+    }
+
+    if (this->collisionCounter >= 8) {
+        //CutsceneManager_Queue(this->dyna.actor.csId);
+        func_80AF3B8C(this);
+    } else {
+        CollisionCheck_SetAC(play, &play->colChkCtx, &this->collider.base);
+    }
+}
+
+void func_80AF3B8C(ObjLightblock* this) {
+    this->actionFunc = func_80AF3BA0;
+}
+
+void func_80AF3BA0(ObjLightblock* this, PlayState* play) {
+        func_80AF3910(this, play);
+        func_80AF3C18(this);
+        Flags_SetSwitch(play, LIGHTBLOCK_DESTROYED(&this->dyna.actor));
+
+        // Possibly points the camera to this actor
+        OnePointCutscene_Attention(play, &this->dyna.actor);
+        // "◯◯◯◯ Sun Block Attention Camera (frame counter  %d)\n"
+        //osSyncPrintf("◯◯◯◯ Time Block 注目カメラ (frame counter  %d)\n", play->state.frames);
+}
+
+void func_80AF3C18(ObjLightblock* this) {
+    this->timer = 80;
+    this->actionFunc = func_80AF3C34;
+}
+
+void func_80AF3C34(ObjLightblock* this, PlayState* play) {
+    //s8 csId;
+
+    this->timer--;
+    if (this->timer <= 0) {
+        //csId = this->dyna.actor.csId;
+        //CutsceneManager_Stop(csId);
+        Actor_Kill(&this->dyna.actor);
+        return;
+    }
+
+    if (this->timer <= 60) {
+        if (this->alpha > 40) {
+            this->alpha -= 40;
+        } else {
+            this->alpha = 0;
+            this->dyna.actor.draw = NULL;
+            DynaPoly_DisableCollision(play, &play->colCtx.dyna, this->dyna.bgId);
+        }
+    }
+}
+
+void ObjLightblock_Update(Actor* thisx, PlayState* play) {
+    ObjLightblock* this = THIS;
+
+    this->actionFunc(this, play);
+}
+
+void ObjLightblock_Draw(Actor* thisx, PlayState* play) {
+    ObjLightblock* this = THIS;
+
+    OPEN_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
+
+    if (this->alpha < 255) {
+        Gfx_SetupDL_25Xlu(play->state.gfxCtx);
+        gSPSegment(POLY_XLU_DISP++, 0x08, D_80116280);
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(play->state.gfxCtx, __FILE__, __LINE__), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0x80, 255, 255, 255, this->alpha);
+        gSPDisplayList(POLY_XLU_DISP++, gSunBlockDL);
+    } else {
+        Gfx_SetupDL_25Opa(play->state.gfxCtx);
+        gSPSegment(POLY_OPA_DISP++, 0x08, &D_80116280[2]);
+        gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(play->state.gfxCtx, __FILE__, __LINE__), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gDPSetPrimColor(POLY_OPA_DISP++, 0, 0x80, 255, 255, 255, 255);
+        gSPDisplayList(POLY_OPA_DISP++, gSunBlockDL);
+    }
+
+    CLOSE_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
+}
---
+++ CustomActors/LightBlock/src/actor/0x01E4-LightBlock/z_obj_lightblock.h
@@ -0,0 +1,27 @@
+#ifndef Z_OBJ_LIGHTBLOCK_H
+#define Z_OBJ_LIGHTBLOCK_H
+
+#include "global.h"
+
+#define LIGHTBLOCK_TYPE(thisx) ((thisx)->params & 1)
+#define LIGHTBLOCK_DESTROYED(thisx) (((thisx)->params >> 8) & 0x7F)
+
+struct ObjLightblock;
+
+typedef void (*ObjLightblockActionFunc)(struct ObjLightblock*, PlayState*);
+
+typedef struct ObjLightblock {
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ ColliderCylinder collider;
+    /* 0x1A8 */ ObjLightblockActionFunc actionFunc;
+    /* 0x1AC */ s8 timer;
+    /* 0x1AD */ u8 alpha;
+    /* 0x1AE */ s8 collisionCounter;
+} ObjLightblock; // size = 0x1B0
+
+#define gSunBlockEmptyDL 0x06000170
+#define gSunBlockDL 0x06000178
+#define gSunBlockTLUT 0x06000258
+#define gSunBlockTex 0x06000278
+#define gSunBlockCol 0x06000B80
+#endif // Z_OBJ_LIGHTBLOCK_H
